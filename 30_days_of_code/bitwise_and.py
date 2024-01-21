a3d3b1a09a2d7866d5b379fdc55a76feb0a5833a
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-01-21 06:54:16 +0300

    fix: :bug: https://www.hackerrank.com/challenges/30-bitwise-and/problem

--- 30_days_of_code/bitwise_and.py
+++ 30_days_of_code/bitwise_and.py
@@ -1,5 +1,22 @@
 # https://www.hackerrank.com/challenges/30-bitwise-and/problem?isFullScreen=true
 
+def bitwiseAnd(N, K):
+    res=0
+    for i in range(1, N):
+        for j in range(i + 1, N + 1):
+            a = "{0:b}".format(i)
+            b = "{0:b}".format(j)
+            m= max(len(a), len(b))
+            a=a.rjust(m, "0")
+            b=b.rjust(m, "0")
+            s=""
+            for k in range(m):
+                s = str(int(a[k]) * int(b[k])) + s
+            v=int(s, 2)
+            if v > res and v < K: res = v
+    return res
+
+""" # Time limit exceeded
 def toBinary(n):
     res=""
     r = n
@@ -23,7 +40,7 @@ def bitwiseAnd(N, K):
                 s = str(int(a[k]) * int(b[k])) + s
             v=int(s, 2)
             if v > res and v < K: res = v
-    return res
+    return res """
 
 a = bitwiseAnd(2,2)
 print(a)
